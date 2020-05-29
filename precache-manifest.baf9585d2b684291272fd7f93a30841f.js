9d7c457487f8af4ada22f62655222077fbbffd01
zhangting5 <zhangting5@xiaomi.com>
2020-05-29 18:14:29 +0800

    Updates

---
+++ precache-manifest.baf9585d2b684291272fd7f93a30841f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d02f49e9bdaa614c61e184b6743b14c6",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "cf99a4c6db6f63ab8d5d",
+    "url": "/react-tutorial/static/css/main.f3900a30.chunk.css"
+  },
+  {
+    "revision": "2e0ae2d37bdcb1db831c",
+    "url": "/react-tutorial/static/js/2.4b9d3397.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.4b9d3397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf99a4c6db6f63ab8d5d",
+    "url": "/react-tutorial/static/js/main.82b41896.chunk.js"
+  },
+  {
+    "revision": "143094d949d4d9b161c1",
+    "url": "/react-tutorial/static/js/runtime-main.174060d8.js"
+  }
+]);
